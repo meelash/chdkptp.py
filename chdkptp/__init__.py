bace5e72642bbaf0a9e7db16f2aa1b6a8f2bed24
Saleem Abdul Hamid <meelash@gmail.com>
2018-03-20 21:15:57 +0500

    Expose PTPError exception class so they can be handled by client libraries nicely. Since they have special properties like the PTP traceback.

--- chdkptp/__init__.py
+++ chdkptp/__init__.py
@@ -1,4 +1,5 @@
 from chdkptp.device import ChdkDevice, list_devices, DeviceInfo
+from chdkptp.lua import PTPError
 
 __version__ = "0.1.3"
-__all__ = ['ChdkDevice', 'list_devices', 'DeviceInfo']
+__all__ = ['ChdkDevice', 'list_devices', 'DeviceInfo', 'PTPError']
